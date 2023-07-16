77409bca093b4c5846dc779a7996c966e38de28b
bolivaraponte <apontebolivar@gmail.com>
2023-07-16 00:19:10 -0500

    Name and last names are in bold now. Bibtex needs to have names with correct diacritics (e.g. ó).

--- lua/strong2.lua
+++ lua/strong2.lua
@@ -2,13 +2,13 @@ function Block(el)
     if el.t == "Para" or el.t == "Plain" then
         for k, _ in ipairs(el.content) do
 
-            if el.content[k].t == "Str" and el.content[k].text == "Aponte," and
+            if el.content[k].t == "Str" and el.content[k].text == "Aponte" and
                 el.content[k + 1].t == "Space" and el.content[k + 2].t == "Str" and el.content[k + 2].text == "Rolon," and
-                el.content[k + 2].t == "Space" and el.content[k + 3].t == "Str" and
-                el.content[k + 3].text:find("^B.") then
+                el.content[k + 3].t == "Space" and el.content[k + 4].t == "Str" and
+                el.content[k + 4].text:find("^B") then
 
-                local _, e = el.content[k + 3].text:find("^B.")
-                local rest = el.content[k + 3].text:sub(e + 1) -- empty if e+1>length
+                local _, e = el.content[k + 4].text:find("^B")
+                local rest = el.content[k + 4].text:sub(e + 1) -- empty if e+1>length
                 el.content[k] = pandoc.Strong {pandoc.Str("Aponte Rolon, B.")}
                 el.content[k + 1] = pandoc.Str(rest)
                 table.remove(el.content, k + 2) -- safe? another way would be to set element k+2 to Str("")
